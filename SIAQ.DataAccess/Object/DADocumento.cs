47177e881dc7385fd8802d7e24ed7a280cc04a2f
carlosvalladolid <lachoman@hotmail.com>
2014-03-06 13:04:37 -0600

    Agregué una entidad para los documentos

--- SIAQ.DataAccess/Object/DADocumento.cs
+++ SIAQ.DataAccess/Object/DADocumento.cs
@@ -1,11 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
+using SIAQ.Entity.Object;
+
 namespace SIAQ.DataAccess.Object
 {
-    public class DADocumento
+    public class DADocumento : DABase
     {
+        protected int _ErrorId;
+        protected string _ErrorDescription;
+
+        /// <summary>
+        ///     Número de error, en caso de que haya ocurrido uno. Cero por default.
+        /// </summary>
+        public int ErrorId
+        {
+            get { return _ErrorId; }
+        }
+
+        /// <summary>
+        ///     Descripción de error, en caso de que haya ocurrido uno. Empty por default.
+        /// </summary>
+        public string ErrorDescription
+        {
+            get { return _ErrorDescription; }
+        }
+
+        #region "Method"
+            public DataSet SelectDocumento(ENTDocumento ENTDocumento, string ConnectionString)
+            {
+                DataSet ResultData = new DataSet();
+                SqlConnection Connection = new SqlConnection(ConnectionString);
+                SqlCommand Command;
+                SqlParameter Parameter;
+                SqlDataAdapter DataAdapter;
+
+                try
+                {
+                    Command = new SqlCommand("SelectDocumento", Connection);
+                    Command.CommandType = CommandType.StoredProcedure;
+
+                    Parameter = new SqlParameter("SolicitudId", SqlDbType.Int);
+                    Parameter.Value = ENTDocumento.SolicitudId;
+                    Command.Parameters.Add(Parameter);
+
+                    DataAdapter = new SqlDataAdapter(Command);
+
+                    Connection.Open();
+                    DataAdapter.Fill(ResultData);
+                    Connection.Close();
+
+                    return ResultData;
+                }
+                catch (SqlException Exception)
+                {
+                    _ErrorId = Exception.Number;
+                    _ErrorDescription = Exception.Message;
+
+                    if (Connection.State == ConnectionState.Open)
+                        Connection.Close();
+
+                    return ResultData;
+                }
+            }
+        #endregion
     }
 }
---
+++ SIAQ.Entity/Object/ENTDocumento.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace SIAQ.Entity.Object
+{
+    public class ENTDocumento : ENTBase
+    {
+        private string _RepositorioId;
+        private int _SolicitudId;
+        private int _ExpedienteId;
+        private int _RecomendacionId;
+        private int _TipoDocumentoId;
+        private int _UsuarioIdInsert;
+        private int _UsuarioIdLastUpdate;
+        private string _Nombre;
+        private string _Descripcion;
+        private byte[] _Documento;
+        private FileUpload _FileUpload;
+        private DataSet _ResultData;
+
+        public ENTDocumento()
+        {
+            _RepositorioId = string.Empty;
+            _SolicitudId = 0;
+            _ExpedienteId = 0;
+            _RecomendacionId = 0;
+            _TipoDocumentoId = 0;
+            _UsuarioIdInsert = 0;
+            _UsuarioIdLastUpdate = 0;
+            _Nombre = string.Empty;
+            _Descripcion = string.Empty;
+            _Documento = new byte[] { 0 };
+            _FileUpload = null;
+            _ResultData = null;
+        }
+
+        /// <summary>
+        ///     Identificador del documento.
+        /// </summary>
+        public string RepositorioId
+        {
+            get { return _RepositorioId; }
+            set { _RepositorioId = value; }
+        }
+
+        /// <summary>
+        ///     Identificador de la solicitud.
+        /// </summary>
+        public int SolicitudId
+        {
+            get { return _SolicitudId; }
+            set { _SolicitudId = value; }
+        }
+
+        /// <summary>
+        ///     Identificador del expediente.
+        /// </summary>
+        public int ExpedienteId
+        {
+            get { return _ExpedienteId; }
+            set { _ExpedienteId = value; }
+        }
+
+        /// <summary>
+        ///     Identificador de la recomendación.
+        /// </summary>
+        public int RecomendacionId
+        {
+            get { return _RecomendacionId; }
+            set { _RecomendacionId = value; }
+        }
+
+        /// <summary>
+        ///     Identificador del tipo de documento.
+        /// </summary>
+        public int TipoDocumentoId
+        {
+            get { return _TipoDocumentoId; }
+            set { _TipoDocumentoId = value; }
+        }
+
+        /// <summary>
+        ///     Identificador del usuario que creó el registro.
+        /// </summary>
+        public int UsuarioIdInsert
+        {
+            get { return _UsuarioIdInsert; }
+            set { _UsuarioIdInsert = value; }
+        }
+
+        /// <summary>
+        ///     Identificador del último usuario que modificó el registro.
+        /// </summary>
+        public int UsuarioIdLastUpdate
+        {
+            get { return _UsuarioIdLastUpdate; }
+            set { _UsuarioIdLastUpdate = value; }
+        }
+    }
+}

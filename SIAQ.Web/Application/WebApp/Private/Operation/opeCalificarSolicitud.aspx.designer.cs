927cc7176daac4966e2ae3b495ff060390853c52
Felipeveliz <felipe.veliz@gmail.com>
2014-03-28 16:03:36 -0600

    corrección ope Calificar Solitud

--- SIAQ.Web/Application/WebApp/Private/Operation/opeCalificarSolicitud.aspx.designer.cs
+++ SIAQ.Web/Application/WebApp/Private/Operation/opeCalificarSolicitud.aspx.designer.cs
@@ -22,15 +22,6 @@ namespace SIAQ.Web.Application.WebApp.Private.Operation {
         protected global::System.Web.UI.WebControls.Label SolicitudLabel;
         
         /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
         /// ddlCalificacion control.
         /// </summary>
         /// <remarks>

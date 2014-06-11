bb9bb932862b670fddb3a883e71a351af079bfd4
kobbee <kobbee13@gmail.com>
2014-06-11 18:10:52 -0500

    Subida a espera de nuevos archivos

--- SIAQ.DataAccess/Object/DAArchivoExpediente.cs
+++ SIAQ.DataAccess/Object/DAArchivoExpediente.cs
@@ -1,11 +1,89 @@
-﻿using System;
+﻿/*---------------------------------------------------------------------------------------------------------------------------------
+' Clase: DAArchivoExpediente
+' Autor: Ruben.Cobos
+' Fecha: 11-Junio-2014
+'
+' Proposito:
+'          Clase que modela la capa de capa de acceso a datos de la aplicación con métodos relacionados con los ArchivoExpedientees
+'----------------------------------------------------------------------------------------------------------------------------------*/
+
+// Referencias
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+
+// Referencias manuales
+using System.Data;
+using System.Data.SqlClient;
+using SIAQ.Entity.Object;
 
 namespace SIAQ.DataAccess.Object
 {
-	class DAArchivoExpediente
+
+	public class DAArchivoExpediente : DABase
 	{
+
+
+		///<remarks>
+		///   <name>DAArchivoExpediente.SelectArchivoExpediente</name>
+		///   <create>11-Junio-2014</create>
+		///   <author>Ruben.Cobos</author>
+		///</remarks>
+		///<summary>Obtiene un listado de expedientes existentes en el sistema para el módulo de Archivos en base a los parámetros proporcionados</summary>
+		///<param name="oENTArchivoExpediente">Entidad de ArchivoExpediente con los parámetros necesarios para consultar la información</param>
+		///<param name="sConnection">Cadena de conexión a la base de datos</param>
+		///<param name="iAlternateDBTimeout">Valor en milisegundos del Timeout en la consulta a la base de datos. 0 si se desea el Timeout por default</param>
+		///<returns>Una entidad de respuesta</returns>
+		public ENTResponse SelectArchivoExpediente(ENTArchivoExpediente oENTArchivoExpediente, String sConnection, Int32 iAlternateDBTimeout){
+			SqlConnection sqlCnn = new SqlConnection(sConnection);
+			SqlCommand sqlCom;
+			SqlParameter sqlPar;
+			SqlDataAdapter sqlDA;
+
+			ENTResponse oENTResponse = new ENTResponse();
+
+			// Configuración de objetos
+			sqlCom = new SqlCommand("uspExpediente_Sel_Archivo_Filtro", sqlCnn);
+			sqlCom.CommandType = CommandType.StoredProcedure;
+
+			// Timeout alternativo en caso de ser solicitado
+			if (iAlternateDBTimeout > 0) { sqlCom.CommandTimeout = iAlternateDBTimeout; }
+
+			// Parametros
+			sqlPar = new SqlParameter("idUsuario", SqlDbType.Int);
+			sqlPar.Value = oENTArchivoExpediente.idUsuario;
+			sqlCom.Parameters.Add(sqlPar);
+
+			sqlPar = new SqlParameter("Numero", SqlDbType.VarChar);
+			sqlPar.Value = oENTArchivoExpediente.Numero;
+			sqlCom.Parameters.Add(sqlPar);
+
+			sqlPar = new SqlParameter("Quejoso", SqlDbType.VarChar);
+			sqlPar.Value = oENTArchivoExpediente.Quejoso;
+			sqlCom.Parameters.Add(sqlPar);
+
+			// Inicializaciones
+			oENTResponse.dsResponse = new DataSet();
+			sqlDA = new SqlDataAdapter(sqlCom);
+
+			// Transacción
+			try{
+				sqlCnn.Open();
+				sqlDA.Fill(oENTResponse.dsResponse);
+				sqlCnn.Close();
+			}catch (SqlException sqlEx){
+				oENTResponse.ExceptionRaised(sqlEx.Message);
+			}catch (Exception ex){
+				oENTResponse.ExceptionRaised(ex.Message);
+			}finally{
+				if (sqlCnn.State == ConnectionState.Open) { sqlCnn.Close(); }
+				sqlCnn.Dispose();
+			}
+
+			// Resultado
+			return oENTResponse;
+		}
+
 	}
+
 }
--- SIAQ.Entity/Object/ENTArchivoExpediente.cs
+++ SIAQ.Entity/Object/ENTArchivoExpediente.cs
@@ -1,11 +1,78 @@
-﻿using System;
+﻿/*---------------------------------------------------------------------------------------------------------------------------------
+' Clase: ENTArchivoExpediente
+' Autor: Ruben.Cobos
+' Fecha: 11-Junio-2014
+'
+' Proposito:
+'          Clase que modela el catálogo de ArchivoExpedientees de la aplicación
+'----------------------------------------------------------------------------------------------------------------------------------*/
+
+// Referencias
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace SIAQ.Entity.Object
 {
-	class ENTArchivoExpediente
+
+	public class ENTArchivoExpediente : ENTBase
 	{
+
+		// Definiciones
+		private Int32	_idUsuario;	// Identificador único del Usuario que tiene asignado el Expediente
+		private String	_Numero;	// Número de expediente a filtrar
+		private String	_Quejoso;	// Nombre del quejoso que levanta la denuncia
+
+		
+		 //Constructor
+
+		public ENTArchivoExpediente()
+		{
+			_idUsuario = 0;
+			_Numero = "";
+			_Quejoso = "";
+		}
+
+
+		// Propiedades
+
+		///<remarks>
+		///   <name>ENTArchivoExpediente.idUsuario</name>
+		///   <create>11-Junio-2014</create>
+		///   <author>Ruben.Cobos</author>
+		///</remarks>
+		///<summary>Obtiene/Asigna el identificador único del Usuario que tiene asignado el Expediente</summary>
+		public Int32 idUsuario
+		{
+			get { return _idUsuario; }
+			set { _idUsuario = value; }
+		}
+
+		///<remarks>
+		///   <name>ENTArchivoExpediente.Numero</name>
+		///   <create>11-Junio-2014</create>
+		///   <author>Ruben.Cobos</author>
+		///</remarks>
+		///<summary>Obtiene/Asigna el número de expediente a filtrar</summary>
+		public String Numero
+		{
+			get { return _Numero; }
+			set { _Numero = value; }
+		}
+
+		///<remarks>
+		///   <name>ENTArchivoExpediente.Quejoso</name>
+		///   <create>11-Junio-2014</create>
+		///   <author>Ruben.Cobos</author>
+		///</remarks>
+		///<summary>Obtiene/Asigna el nombre del quejoso que levanta la denuncia</summary>
+		public String Quejoso
+		{
+			get { return _Quejoso; }
+			set { _Quejoso = value; }
+		}
+		
+
 	}
 }
